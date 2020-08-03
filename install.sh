655885e31d8317e8ed5704ce5bd93841bad6378d
juewuy <juewuy@126.com>
2020-08-03 15:35:45 +0800

    正式版

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ echo -e "\033[37m 目前仅支持小米AX系列3款路由器"
 echo -e "\033[44m 其余型号可到TG群报名参与测试\033[0m"
 echo -----------------------------------------------
 echo -e "\033[32m1 在默认目录(/etc)安装Clash for Miwifi"
-echo -e "\033[33m2 手动设置安装目录（请不要设为/tmp,重启会消失）"
+echo -e "\033[33m2 手动设置安装目录（小白慎用！）"
 echo -e "\033[0m0 退出安装"
 echo -----------------------------------------------
 read -p "请输入相应数字 > " num
@@ -23,11 +23,12 @@ if [ -z $num ];then
 	echo 安装已取消
 	exit;
 elif [[ $num == 1 ]];then
-	clashdir=/etc/clash
+	dir=/etc
 elif [[ $num == 2 ]];then
 	echo -----------------------------------------------
 	echo '可用路径 剩余空间:'
 	df -h | awk '{print $6,$2}'| sed 1d 
+	echo '路径是必须带 / 的格式，写入虚拟内存(/tmp,/sys,..)的文件会在重启后消失！！！'
 	read -p "请输入自定义路径 > " dir
 	if [ -z $dir ];then
 		echo 路径错误！已取消安装！
@@ -37,13 +38,10 @@ else
 	echo 安装已取消
 	exit;
 fi
-
-
+#下载文件包
+echo -----------------------------------------------
 echo 开始从服务器获取安装文件！
 tarurl=$url/bin/clashfm.tar.gz
-
-
-
 if command -v curl &> /dev/null; then
 	result=$(curl -w %{http_code} -skLo /tmp/clashfm.tar.gz $tarurl)
 else
@@ -51,21 +49,27 @@ else
 	[ $? -eq 0 ] && result="200"
 fi
 [ "$result" != "200" ] && echo "文件下载失败！" && exit 1
-
-
+#解压
+echo -----------------------------------------------
 echo 开始解压文件！
-tar -zxvf /tmp/clashfm.tar.gz -C $dir > /dev/null
+tar -zxvf '/tmp/clashfm.tar.gz' -C $dir > /dev/null
 [ $? -ne 0 ] && echo "文件解压失败！" && exit 1 
-
+#初始化文件目录
 mv $dir/clashservice /etc/init.d/clash #将clash服务文件移动到系统目录
 chmod  777 $dir/clash  #授予权限
 chmod  777 /etc/init.d/clash #授予权限
+fi
+#设置环境变量
 sed -i '/alias clash=*/'d /etc/profile
-sed -i '$aalias\ clash=\"sh \/etc\/clash\/clash.sh\"' /etc/profile
-alias clash="sh /etc/clash/clash.sh" #设置系统变量
-
-rm -rf /tmp/clashfm.tar.gz #删除临时文件
-
+echo "alias clash=\"sh $dir/clash/clash.sh\"" >> /etc/profile
+alias clash="sh $dir/clash/clash.sh" #设置快捷命令环境变量
+sed -i '/export clashdir=*/'d /etc/profile
+echo "export clashdir=\"$dir/clash\"" >> /etc/profile
+export clashdir="$dir/clash" #设置clash路径环境变量
+#删除临时文件
+rm -rf /tmp/clashfm.tar.gz 
+#提示
+echo -----------------------------------------------
 echo clash for Miwifi 已经安装成功!
-echo -e "\033[33m直接输入\033[32mclash命令即可管理！！！\033[0m"
+echo -e "\033[33m直接输入\033[30;47m clash \033[0;33m命令即可管理！！！\033[0m"
 
