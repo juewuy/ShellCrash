f3eb5c16c89a833786ed0b8955256fb10f3a3e52
Bot <juewuy@gmail.com>
2022-06-18 09:02:13 +0000

    自动更新最新Dashboard、地址库、根证书

---
+++ bin/Yacd-meta-gh-pages/assets/Select.763fb4e2.js
@@ -0,0 +1,1 @@
+import{j as c}from"./index.1ca2d13d.js";const n="_select_gfkcv_1";var o={select:n};function i({options:s,selected:t,onChange:l}){return c("select",{className:o.select,value:t,onChange:l,children:s.map(([e,r])=>c("option",{value:e,children:r},e))})}export{i as S};
