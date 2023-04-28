087ef4636f52e55f684407993801e547cd1524c9
Lemoe <liukangdev@gmail.com>
2023-04-28 11:11:26 +0800

    修复小米设备开机初始化完成的判断

--- scripts/misnap_init.sh
+++ scripts/misnap_init.sh
@@ -46,15 +46,8 @@ init(){
 	chmod 755 /etc/init.d/clash
 	#启动服务
 	if [ ! -f $clashdir/.dis_startup ]; then
-		log_file=$(uci get system.@system[0].log_file)
-		while [ "$i" -lt 10 ]; do
-			sleep 5
-			[ -n "$(grep 'init complete' $log_file)" ] && i=10 || i=$((i + 1))
-		done
 		#AX6S/AX6000修复tun功能
 		[ -f $clashdir/tun.ko -a ! -f /lib/modules/4.4.198/tun.ko ] && tunfix
-		#启动延迟
-		sleep 60
 		#启动服务
 		/etc/init.d/clash start
 		/etc/init.d/clash enable
