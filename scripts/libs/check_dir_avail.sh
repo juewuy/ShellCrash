93ac1b1da94e54eac973e879d57532afe6f7ad58
juewuy <juewuy@gmail.com>
2026-01-06 13:45:20 +0800

    ~优化dir_avail函数

--- scripts/libs/check_dir_avail.sh
+++ scripts/libs/check_dir_avail.sh
@@ -1,5 +1,5 @@
 #检查目录剩余空间——$1:目标路径 $2:-h参数
-dir_avail(){
+dir_avail() {
 	df -h >/dev/null 2>&1 && h="$2"
-	df $h "$1" |awk '{ for(i=1;i<=NF;i++){ if(NR==1){ arr[i]=$i; }else{ arr[i]=arr[i]" "$i; } } } END{ for(i=1;i<=NF;i++){ print arr[i]; } }' |grep -E 'Ava|可用' |awk '{print $2}'
+    df -P $2 "${1:-.}" 2>/dev/null | awk 'NR==2 {print $(NF-2)}'
 }
