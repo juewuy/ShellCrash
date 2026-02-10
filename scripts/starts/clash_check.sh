d86969bf9b93af3c65393ec09499e5078e6ae59f
juewuy <juewuy@gmail.com>
2026-02-10 19:02:54 +0800

    ~修复Mihomo内核自动检测下载geoip和geosite数据库功能未工作的bug

--- scripts/starts/clash_check.sh
+++ scripts/starts/clash_check.sh
@@ -11,10 +11,10 @@ clash_check() { #clash启动前检查
     [ "$crashcore" = "clash" ] && [ "$firewall_area" = 2 -o "$firewall_area" = 3 ] && [ -z "$(grep '0:7890' /etc/passwd)" ] &&
         core_exchange meta '当前内核不支持非root用户启用本机代理'
     check_core
-    #预下载GeoIP数据库并排除存在自定义数据库链接的情况
-    [ -n "$(grep -oEi 'geoip:' "$CRASHDIR"/yamls/config.yaml)" ] && check_geo Country.mmdb cn_mini.mmdb
-    #预下载GeoSite数据库并排除存在自定义数据库链接的情况
-    [ -n "$(grep -oEi 'geosite:' "$CRASHDIR"/yamls/config.yaml)" ] && check_geo GeoSite.dat geosite.dat
+    #预下载GeoIP数据库
+    grep -qEi 'GEOIP,CN' "$CRASHDIR"/yamls/config.yaml && ! grep -Eq '^[[:space:]]*geodata-mode:' "$CRASHDIR"/yamls/*.yaml && check_geo Country.mmdb cn_mini.mmdb
+    #预下载GeoSite数据库
+    grep -qEi 'GEOSITE,' "$CRASHDIR"/yamls/config.yaml && ! grep -Eq '^[[:space:]]*geosite:' "$CRASHDIR"/yamls/*.yaml && check_geo GeoSite.dat geosite.dat
     #预下载cn.mrs数据库
     [ "$dns_mod" = "mix" ] || [ "$dns_mod" = "route" ] && ! grep -Eq '^[[:space:]]*cn:' "$CRASHDIR"/yamls/*.yaml && check_geo ruleset/cn.mrs mrs_geosite_cn.mrs
     return 0
